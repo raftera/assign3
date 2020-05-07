b0e022a28033db178961a696fc418417b387be30
raftera <63008996+raftera@users.noreply.github.com>
2020-05-07 01:49:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,39 @@
  * Add your JavaScript to this file to complete the assignment.
  */
 
-var maketwit = document.querySelector('create-twit-button');
-maketwit.addEventListener('click', function()){
-  console.log("== Make twit button was clicked")
+document.getElementById("create-twit-button").addEventListener("click", createTwit);
+
+var author = '';
+var content = '';
+
+function createTwit(){
+  console.log("==create twit clicked");
+  var modal = 0;
+  var modal = document.getElementsByClassName("hidden");
+  for (var i = 0; i < modal.length; i+=1){
+    modal[i].style.display = 'block';
+  }
+  //document.getElementsByClassName("hidden").style.display = "block";
+}
+
+document.getElementsByClassName("modal-close-button")[0].addEventListener("click", closeCreate);
+document.getElementsByClassName("modal-cancel-button")[0].addEventListener("click", closeCreate);
+
+function closeCreate() {
+  console.log("==close twit creator clicked");
+  var modal = document.getElementsByClassName("hidden");
+  for (var i = 0; i < modal.length; i+=1){
+  modal[i].style.display = 'none';
+  }
+  author = '';
+  content = '';
+
+}
+
+document.getElementsByClassName("modal-accept-button")[0].addEventListener("click", twitData);
+
+function twitData(){
+  
 }
 
 function addTwit(content, author) {
@@ -27,5 +57,7 @@ function addTwit(content, author) {
   newTwit.classList.add('twit')
 
   var img = document.createElement('div');
-  img.classList.add("")
+  img.classList.add("");
+
+
 }
